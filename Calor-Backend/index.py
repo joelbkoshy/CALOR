ee57ad6c2a80b2e18af63470aa02288d7049dde3
joelbkoshy <joelbkoshy@gmail.com>
2023-12-19 10:23:29 +0530

    Done evaluation

--- Calor-Backend/index.py
+++ Calor-Backend/index.py
@@ -8,6 +8,7 @@ import traceback
 import asyncio
 from pymongo import MongoClient,DESCENDING
 import uuid
+import google.generativeai as genai
 
 
 from bardapi import Bard
@@ -20,6 +21,38 @@ import os
 # set your __Secure-1PSID value to key
 # token = 'dwgz9hbe6wWwR0o9qy_5DJbo2znNxcMxUaN_kYsAVHURp86e1FH94PBF_PiQOELKCV2KKQ.'
 
+genai.configure(api_key=os.environ.get("GENAI_API_KEY"))
+
+# Set up the model
+generation_config = {
+  "temperature": 0.9,
+  "top_p": 1,
+  "top_k": 1,
+  "max_output_tokens": 2048,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  }
+]
+
+model = genai.GenerativeModel(model_name="gemini-pro",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
 
 
 
@@ -51,31 +84,32 @@ async def forward_asr_request():
                 print("Going to send the request")
                 response = await client.post(target_url, files=files, headers=headers)
                 response_text = response.text
-                print("Current Working Directory:", os.getcwd())
-                # set your input text
-                # input_text = ""
-
-# Send an API request and get a response.
-                # bard = Bard(token=token)
-                # bard_response=bard.get_answer(response_text)['content']
+                prompt_parts = [  " ",
+  "input: Act like a therapist, being friendly to the user, and asking his concerns seriously like his or hers best friend",
+  "output: "]   
+                prompt_parts.append(f"input : {response_text}")
+                prompt_parts.append("output : ")
+                Calorresponse = model.generate_content(prompt_parts)
+                Calorresponse=Calorresponse.text
+        #         print("Current Working Directory:", os.getcwd())
                 
-                with open('D:\\MCA\\5th_TRIMESTER\\SPECIALIZATION_PROJECT\\CALOR\\Calor-Backend\\models\\distilbert_model.pkl', 'rb') as model_file, \
-                open('D:\\MCA\\5th_TRIMESTER\\SPECIALIZATION_PROJECT\\CALOR\\Calor-Backend\\models\\distilbert_tokenizer.pkl', 'rb') as tokenizer_file, \
-                open('D:\\MCA\\5th_TRIMESTER\\SPECIALIZATION_PROJECT\\CALOR\\Calor-Backend\\models\\responses.pkl', 'rb') as responses_file:
-                 loaded_model = pickle.load(model_file)
-                 loaded_tokenizer = pickle.load(tokenizer_file)
-                 loaded_responses = pickle.load(responses_file)
-           # Test the loaded model and tokenizer with a longer sentence
-                 input_ids = loaded_tokenizer.encode(response_text, return_tensors='pt', max_length=512, truncation=True)
+        #         with open('D:\\MCA\\5th_TRIMESTER\\SPECIALIZATION_PROJECT\\CALOR\\Calor-Backend\\models\\distilbert_model.pkl', 'rb') as model_file, \
+        #         open('D:\\MCA\\5th_TRIMESTER\\SPECIALIZATION_PROJECT\\CALOR\\Calor-Backend\\models\\distilbert_tokenizer.pkl', 'rb') as tokenizer_file, \
+        #         open('D:\\MCA\\5th_TRIMESTER\\SPECIALIZATION_PROJECT\\CALOR\\Calor-Backend\\models\\responses.pkl', 'rb') as responses_file:
+        #          loaded_model = pickle.load(model_file)
+        #          loaded_tokenizer = pickle.load(tokenizer_file)
+        #          loaded_responses = pickle.load(responses_file)
+        #    # Test the loaded model and tokenizer with a longer sentence
+        #          input_ids = loaded_tokenizer.encode(response_text, return_tensors='pt', max_length=512, truncation=True)
 
-                with torch.no_grad():
-                 output = loaded_model(input_ids)
+        #         with torch.no_grad():
+        #          output = loaded_model(input_ids)
 
-                probs = torch.softmax(output.logits, dim=1)
-                predicted_class = torch.argmax(probs).item()
+        #         probs = torch.softmax(output.logits, dim=1)
+        #         predicted_class = torch.argmax(probs).item()
 
-            # Get the predicted response using the predicted class as an index
-                predicted_response = loaded_responses[predicted_class]
+        #     # Get the predicted response using the predicted class as an index
+        #         predicted_response = loaded_responses[predicted_class]
 
             # Save the response text as a message in MongoDB
                 message_id = str(uuid.uuid4())
@@ -84,7 +118,7 @@ async def forward_asr_request():
                 message = {
                 'message_id': message_id,
                 'user_message': response_text,
-                'calor_message': predicted_response,
+                'calor_message': Calorresponse,
                 'timestamp': current_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
                 }
 
@@ -93,7 +127,7 @@ async def forward_asr_request():
                 update = {'$push': {'messages': message}, '$setOnInsert': {'createdAt': datetime.datetime.utcnow()}}
                 collection.update_one(query, update, upsert=True)
 
-            return jsonify({"message_id": message_id, "response_text": response_text,"calor_mesaage":predicted_response})
+            return jsonify({"message_id": message_id, "response_text": response_text,"calor_mesaage":Calorresponse})
         else:
             return jsonify({"error": "No audio file provided"}), 400
 
@@ -104,6 +138,7 @@ async def forward_asr_request():
 
 @app.route('/asr', methods=['POST'])
 def asr_route():
+    print("Going to send request")
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     result = loop.run_until_complete(forward_asr_request())
@@ -126,9 +161,13 @@ def new_Chat():
     
 @app.route('/getChats',methods=['GET'])
 def get_chats():
+    print("came in")
     data=list(collection.find({},{'_id': 0}).sort('createdAt', DESCENDING))
     return  data
 
+@app.route('/')
+def home():
+    return 'Welcome to My Chat Application!'
 
 @app.route('/fetch_chat/<string:chat_id>', methods=['GET'])
 def fetch_chat_messages(chat_id):
@@ -149,6 +188,6 @@ def fetch_chat_messages(chat_id):
         return jsonify({"error": f"Error fetching chat messages: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("FLASK_PORT",8000))
+    port = int(os.environ.get("FLASK_PORT",8800))
     app.run(debug=True, port=port)
     
